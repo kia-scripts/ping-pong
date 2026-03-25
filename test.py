105d2934e7b208d9c99bbe15c4acaad19d7a7c43
Ilia Koptiaev <ilya_koptyaev@mail.ru>
2026-03-25 03:17:49 +0300

    second_dev

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import pygame
+import random
 print('ping-pong')
 print('новый')
 print('ещё правка')
-a = 5 
+if random.randint(1,4) == 5:
+    a = 5 
+else:
+    a = 6
+    
 if a == 3: 
     print('вариант 1')
+    b = 10        
 else:
     print('вариант 2')
